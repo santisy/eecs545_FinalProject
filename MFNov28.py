4e6edcda0e0fdc99a4bdf43cd12307de8feb25ba
Cheng-XJTU <couy@umich.edu>
2016-11-30 14:50:52 -0500

    Training part for basic PMF, not accelerated

--- MFNov28.py
+++ MFNov28.py
@@ -96,8 +96,7 @@ if __name__ == "__main__":
         UWriter.writelines(U)
         UWriter.close()
     # at least it worked
-    # testing git
-    # the probl
+
                     
     
                     
