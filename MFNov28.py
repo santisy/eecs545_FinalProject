175832177849f6617e1431d87b7a8bdf9d0f6d64
santisy <bingjilin@me.com>
2016-12-01 00:48:04 -0500

    dec-1-success

--- MFNov28.py
+++ MFNov28.py
@@ -8,6 +8,7 @@ Use five fold data for cross validation
 @author: Cheng Ouyang
 """
 import numpy as np
+import cudamat as cm
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
@@ -18,15 +19,15 @@ import os
 
 if __name__ == "__main__":
     # parameter initialization
-    featureDim = 7 # dimension of feature vector
+    featureDim = 20 # dimension of feature vector
     np.random.seed(0) # make controllable initialization
-    lambdaU = 0.02 # lambda for user matrix regularization
-    lambdaP = 0.02 # lambda for item matrix regularization
-    step = 0.001 # iteration step 
+    lambdaU = 1 # lambda for user matrix regularization
+    lambdaP = 1 # lambda for item matrix regularization
+    step = 0.0002 # iteration step
     # select training data and testing data
     numFolds = 5 # number of folds of data
     testIdx = 1 #index of testing data
-    iterNum = 2 # number of iterations
+    iterNum = 100 # number of iterations
  
     # data file path, and read
     categoryName = 'Shopping'
@@ -34,10 +35,11 @@ if __name__ == "__main__":
     dataTerms = ['userID','itemID','subcategory','rating','time','userLocation','itemLocation','distance']
     now = datetime.datetime.now()        
     resultPath = os.path.expanduser('~/Documents/results/') + str(now.strftime('%m%d%H%M')) + '/'
-    os.mkdir(resultPath)
+    if not os.path.isdir(resultPath):
+        os.mkdir(resultPath)
     # read the raw data into pandas dataframe
     flag = 1
-    for i in range(1,3):
+    for i in range(1, numFolds+1):
         dataFID = dataPath + 'data' + str(i) + '.txt'
         if i!= testIdx:
             if flag == 1:
@@ -57,39 +59,47 @@ if __name__ == "__main__":
     userNum = len(userList)
     # training item ID generation
     itemList = pd.unique(trainContent['itemID']).tolist()
-    itemNum = len(itemList)
-    # training rating matrix construction
+    itemNum = len(itemList) # training rating matrix construction
     gc.enable()
     gc.collect()
     RUI = np.zeros([len(userList),len(itemList)], dtype = np.float16)
     for (idx,row) in trainContent.iterrows():
         RUI[userList.index(row['userID']),itemList.index(row['itemID'])] = np.float16(row['rating'])
     print("RUI has been constructed!")
-    
+    uLst = RUI.nonzero()[0].tolist()
+    iLst = RUI.nonzero()[1].tolist()
+    numRating = len(uLst)
+    ratingList = tuple(zip(uLst, iLst))
+
     # U,P initialization
     U = np.random.randn(len(userList),featureDim)
     P = np.random.randn(len(itemList),featureDim)
     # iteration
-    numRating = np.sum(np.sign(RUI))
-    for i in range(1,iterNum): #use a more rational way, update step by step 
-    # took 220 seconds for one iteration. Try to accelerate by parallel
+    # numRating = np.sum(np.sign(RUI))
+    gc.collect()
+    for i in range(1, iterNum):  # use a more rational way, update step by step
+        # took 5 seconds for one teration now. Excited!
         error = 0
+        count = 0  # for debug
         startTime = time.time()
-        for j in range(0,userNum):
-            for k in range(0,itemNum):
-                if RUI[j][k] != 0:
-                    # update P,U
-                    (Uu,Pi,res) = upUpdate(U[j,np.newaxis],P[k,np.newaxis],RUI[j][k],lambdaU,lambdaP)
-                    U[j] = U[j] - step*Uu
-                    P[k] = P[k] - step*Pi
-                    error += res/numRating
+        print(100)
+        for j, k in ratingList:
+            # count += 1
+            try:
+                RUI[j][k] != 0
+            except:
+                print("RUI == 0, there must be something wrong")
+            (Uu, Pi, res) = upUpdate(U[j, np.newaxis], P[k, np.newaxis], RUI[j][k], lambdaU, lambdaP)
+            U[j] = U[j] - step * Uu
+            P[k] = P[k] - step * Pi
+            error += res / numRating
         iterTime = time.time() - startTime
         print("I have finished one iteration!")
         print("the MAE for objective function is", error)
         print("take", str(iterTime), "seconds")
         # save intermediate results
-        recordCSV(P, resultPath + 'P%s.csv' %i) 
-        recordCSV(U, resultPath + 'U%s.csv' %i)  
+        #recordCSV(P, resultPath + 'P%s.csv' %i)
+        #recordCSV(U, resultPath + 'U%s.csv' %i)
         
     numTest = testContent['userID'].count()
     MSE = 0 # testing error MSE
@@ -107,7 +117,7 @@ if __name__ == "__main__":
             MAE += abs(testRUI - RUI_hat)
         except:
             numInvalid += 1
-            print("invalid testing data detected!")
+            #print("invalid testing data detected!")
             
         if count/1000 == 0:
             print(str((count/numTest)*100) + "%")
@@ -121,8 +131,8 @@ if __name__ == "__main__":
     print(line3)
     resultLogFID = resultPath + "log.txt"
     f = open(resultLogFID, 'w')
-    f.write([line1, '\n', line2, '\n', line3, '\n'])
-    f.write(["feature demention: ", str(featureDim), '\n', "lambdaU: ", str(lambdaU), '\n', "lambdaP: ", str(lambdaP), '\n', "step: ", str(step), '\n'])
+    f.write(line1 + '\n' + line2 + '\n' + line3 + '\n')
+    f.write("feature demention: " + str(featureDim) + '\n' +  "lambdaU: " + str(lambdaU) + '\n' + "lambdaP: " + str(lambdaP) + '\n' + "step: " + str(step) + '\n')
     f.close()
         
         
